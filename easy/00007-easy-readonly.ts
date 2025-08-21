ed8a050aa8fd8e7713c1a61e56ccf111c0f84c75
Vadym Novoselskyi <novoselskyi.vadim@gmail.com>
2025-08-21 16:13:39 +0200

    done 1/2 easy ones

--- easy/00007-easy-readonly.ts
+++ easy/00007-easy-readonly.ts
@@ -31,22 +31,22 @@
 
 /* _____________ Your Code Here _____________ */
 
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+    +readonly [K in keyof T]: T[K];
+};
 
 /* _____________ Test Cases _____________ */
-import type { Equal, Expect } from '@type-challenges/utils'
+import type { Equal, Expect } from "@type-challenges/utils";
 
-type cases = [
-  Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>,
-]
+type cases = [Expect<Equal<MyReadonly<Todo1>, Readonly<Todo1>>>];
 
 interface Todo1 {
-  title: string
-  description: string
-  completed: boolean
-  meta: {
-    author: string
-  }
+    title: string;
+    description: string;
+    completed: boolean;
+    meta: {
+        author: string;
+    };
 }
 
 /* _____________ Further Steps _____________ */
